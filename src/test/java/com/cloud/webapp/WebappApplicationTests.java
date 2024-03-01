c4fffcfe58a1b435882f59457c67508b8a3fbecf
Srikanth <srgunukula@users.noreply.github.com>
2024-03-01 11:24:05 +0530

    Update WebappApplicationTests.java

--- src/test/java/com/cloud/webapp/WebappApplicationTests.java
+++ src/test/java/com/cloud/webapp/WebappApplicationTests.java
@@ -1,13 +1,17 @@
 package com.cloud.webapp;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class WebappApplicationTests {
 
-    @Test
-    void contextLoads() {
-	}
-
+  @Test
+  void contextLoads(ApplicationContext context) {
+    assertThat(context).isNotNull();
+  }
 }
