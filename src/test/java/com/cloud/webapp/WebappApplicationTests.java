4928d2d84e790960f155d64adba325b2d0fe39d8
Srikanth <srgunukula@users.noreply.github.com>
2024-03-01 10:59:59 +0530

    revert the tests file

--- src/test/java/com/cloud/webapp/WebappApplicationTests.java
+++ src/test/java/com/cloud/webapp/WebappApplicationTests.java
@@ -2,28 +2,12 @@ package com.cloud.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
-import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class WebappApplicationTests {
 
     @Test
     void contextLoads() {
-	}
-	@Autowired
-	private TestRestTemplate restTemplate;
-
-	@Test
-	public void homeResponse() {
-		String body = this.restTemplate.getForObject("/", String.class);
-		assertThat(body).isEqualTo("Spring is here!");
 	}
 
 }
