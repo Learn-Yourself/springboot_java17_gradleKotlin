e506506ab08a1cf56a547e460ac4afe450928a54
Srikanth <srgunukula@users.noreply.github.com>
2024-03-01 10:52:26 +0530

    Update WebappApplication.java

--- src/main/java/com/cloud/webapp/WebappApplication.java
+++ src/main/java/com/cloud/webapp/WebappApplication.java
@@ -1,10 +1,17 @@
 package com.cloud.webapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class WebappApplication {
+	
+	@GetMapping("/")
+	String home() {
+		return "Spring is here!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
