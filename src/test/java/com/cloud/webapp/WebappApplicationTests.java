9ad9682df2314d5fdbd28db9e35c8bb8d66ecd27
Srikanth <srgunukula@users.noreply.github.com>
2024-03-01 10:16:12 +0530

    Update WebappApplicationTests.java

--- src/test/java/com/cloud/webapp/WebappApplicationTests.java
+++ src/test/java/com/cloud/webapp/WebappApplicationTests.java
@@ -6,8 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MyController myController;
 
+    @Autowired
+    private MyService myService;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(myController).isNotNull();
+        assertThat(myService).isNotNull();
+    }
 }
